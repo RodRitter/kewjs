721a61a57829812403c0923d9b718f1f3f8bc4fc
Rod Ritter <rod@ritter.co.za>
2015-05-15 15:08:59 +0200

    Added attr() functionality

--- kew.js
+++ kew.js
@@ -11,17 +11,27 @@ var $ = {
 		var selection = (context || document).querySelectorAll(selector);
 		return {
 			list: function() {
+				console.log(selection);
 				return selection;
 			},
-			first: function() {
+			first: function(){
 				return selection[0];
 			},
 			each: function(callback) {
 				for(var i=0; i<selection.length; i++) {
 					callback(selection[i]);
 				}
+			},
+			attr: function(attrName, attrValue) {
+				if(typeof attrName === 'string' && typeof attrValue === 'string') {
+					for (var i = 0; i < selection.length; i++) {
+						var attr = selection[i].getAttribute(attrName) === null ? '' : selection[i].getAttribute(attrName);
+						selection[i].setAttribute(attrName, attr+' '+attrValue);
+					}
+				}
 			}
+
+
 		}
 	}
 }
-
