43a94f71bac8a159f5099f042f1e2f508017d55b
Gabin <hello@gabinaureche.com>
2015-06-15 17:37:49 +0200

    formatting details

--- kew.js
+++ kew.js
@@ -1,45 +1,47 @@
-(function(window) {
-	var $kew = function(selector, context) {
-		if (window === this) {
+(function (window) {
+    var $kew = function (selector, context) {
+        if (window === this) {
             return new $kew(selector);
         }
-		var els = (context || document).querySelectorAll(selector);
-		this.elements = els;
-		this.length = els.length;
-		return this;
-	}
+        var els = (context || document).querySelectorAll(selector);
+        this.elements = els;
+        this.length = els.length;
+        return this;
+    };
 
-	$kew.prototype = {
-		ready: function(callback) {
-			document.addEventListener("DOMContentLoaded", callback);
-		},
-		each: function(callback) {
-			for (var i = 0; i < this.length; i++) {
-				callback(this.elements[i]);
-			}
-		},
-		attr: function(attrName, attrValue) {
-			if (typeof attrName === 'string' && typeof attrValue === 'string') {
-				for (var i = 0; i < this.length; i++) {
-					var attr = '';
-                    
+    $kew.prototype = {
+        ready: function (callback) {
+            document.addEventListener("DOMContentLoaded", callback);
+        },
+        each: function (callback) {
+            for (var i = 0; i < this.length; i++) {
+                callback(this.elements[i]);
+            }
+        },
+        attr: function (attrName, attrValue) {
+            if (typeof attrName === 'string' && typeof attrValue === 'string') {
+                for (var i = 0; i < this.length; i++) {
+                    var attr = '';
+
                     if (this.elements[i].getAttribute(attrName) !== 'undefined') {
                         attr = this.elements[i].getAttribute(attrName);
                     }
 
-					this.elements[i].setAttribute(attrName, attr + ' ' + attrValue);
-				}
-			}
-		},
-		class: function(className) {
-			this.attr('class', className);
-		},
-		remove: function() {
-			for (var i = 0; i < this.length; i++) {
-				this.elements[i].parentElement.removeChild(this.elements[i]);
-			}
-		}
-	}
+                    this.elements[i].setAttribute(attrName, attr + ' ' + attrValue);
+                }
+            }
+        },
 
-	window.$kew = $kew;
+        class: function (className) {
+            this.attr('class', className);
+        },
+
+        remove: function () {
+            for (var i = 0; i < this.length; i++) {
+                this.elements[i].parentElement.removeChild(this.elements[i]);
+            }
+        }
+    };
+
+    window.$kew = $kew;
 })(typeof window != 'undefined' ? window : undefined);
